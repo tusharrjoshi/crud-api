c120899f396ba00ec7d94cf3351150ee5a9890ce
tusharrjoshi <hellotusharr2318@gmail.com>
2022-05-01 09:12:13 +0530

    update 1/05

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(function(req, res, next){
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT ,PATCH , DELETE');
 
-    res.setHeader('Access-Control-Allow-Headers','Origin', 'X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 })
 
